e50a33a27d45b71bcebacd17073e3b8b6408de16
Will Koste <willkoste@gmail.com>
2020-03-28 14:24:07 -0400

    Search Functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends Component{
   state = {
@@ -10,13 +11,22 @@ class App extends Component{
     loading: false
   }
   
-  async componentDidMount(){
+  // async componentDidMount(){ 
+  //   this.setState({loading: true});
+    
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   const data = await res.data;
+    
+  //   this.setState({users: data, loading: false});
+  // }
+
+  searchUsers = async (text) => {
     this.setState({loading: true});
     
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
     const data = await res.data;
-    
-    this.setState({users: data, loading: false});
+
+    this.setState({users: data.items, loading: false});
   }
   
   render(){
@@ -24,6 +34,7 @@ class App extends Component{
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -8,7 +8,7 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
       <h3>{login}</h3>
 
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">Profile</a>
+        <a href={html_url} className="btn btn-dark btn-sm my-1" rel="noopener noreferrer" target="_blank">Profile</a>
       </div>
     </div>
   )
