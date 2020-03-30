ac0e3dd46b52f5d99f0db4b44b7d461217d7c6e2
Will Koste <willkoste@gmail.com>
2020-03-30 11:09:48 -0400

    Alert Context Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {useState, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
-import axios from 'axios';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -9,59 +8,12 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App =() => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  const searchUsers = async (text) => {
-    setLoading(true);
-    
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
-    const data = await res.data;
-
-    setUsers(data.items);
-    setLoading(false);
-  }
-
-  const getUser = async (username) => {
-    setLoading(true);
-    
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
-    const data = await res.data;
-
-    setUser(data);
-    setLoading(false);
-  }
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-    
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); 
-    const data = await res.data;
-
-    setRepos(data);
-    setLoading(false);
-  }
-  
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  }
-
-  const showAlert = (msg, type) => {
-    setAlert({msg, type});
-
-    setTimeout(() => {
-      setAlert(null);
-    }, 2700);
-  }
-  
-  return (
+return (
     <GithubState>
+    <AlertState>
       <Router>
         <div className="App">
           <Navbar />
@@ -69,19 +21,18 @@ const App =() => {
             <Switch>
               <Route path='/' exact render={props => (
                 <Fragment>
-                  <Search searchUsers={searchUsers} clearUsers={clearUsers} setAlert={showAlert} showClear={users.length > 0 ? true : false} />
-                  <Users loading={loading} users={users} />
+                  <Search />
+                  <Users />
                 </Fragment>
               )} />
               <Route path='/about' exact component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={getUser} getUserRepos={getUserRepos} repos={repos} user={user} loading={loading} />
-              )} />
+              <Route exact path='/user/:login' component={User} />
             </Switch>
-            <Alert alert={alert} />
+            <Alert />
           </div>
         </div>
       </Router>
+    </AlertState>
     </GithubState>
   );
 }
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useContext } from 'react';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+const User = ({match}) => {
+  const githubContext = useContext(GithubContext);
+  
+  const {getUser, getUserRepos, loading, user, repos} = githubContext;
+  
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -81,14 +85,6 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
       <Repos repos={repos} />
     </Fragment>
   )
-}
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 }
 
 export default User;
